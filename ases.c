60f6deaa9136e463195f59c76003a1565815524c
SachithKumar0628 <sachith.k@s.amity.edu>
2025-05-07 09:32:07 +0530

    test2 branch

--- ases.c
+++ ases.c
@@ -8,4 +8,14 @@ int pro(int a, int b)
 {
 	return a*b;
 }
-
+int main()
+{
+	int x,y;
+	printf("Enter two numbers");
+	scanf("%d %d",&x,&y);
+	sum(a,b);
+	pro(a,b);
+	printf("sum is %d",sum);
+	printf("product is %d",pro);
+	return 0;
+}
